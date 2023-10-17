9493b12fdc096c792b48f675ffd12101a2093473
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-18 02:21:26 +0600

    a_making component -practice01

--- s4/practice-s4/src/App.js
+++ s4/practice-s4/src/App.js
@@ -1,5 +1,7 @@
 
 import ExpenseItem from "./components/ExpenseItem";
+import PracticeComp1 from "./components/PracticeComp1";
+
 function App() {
   document.getElementById('root')
   return (
@@ -7,6 +9,7 @@ function App() {
       <h2>Let's get started!</h2>
       <p> This is Sohan & React Js </p>
       <ExpenseItem> </ExpenseItem>
+      <PracticeComp1></PracticeComp1>
     </div>
   );
 }
