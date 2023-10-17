96b8c354aec1ec1a44b43395fa5d5a9e4529067f
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-18 02:42:39 +0600

    #3*45_writing more complex jsx code

--- s4/practice-s4/src/App.js
+++ s4/practice-s4/src/App.js
@@ -3,6 +3,7 @@ import ExpenseItem from "./components/ExpenseItem";
 import PracticeComp1 from "./components/PracticeComp1";
 import PracticeComp2 from "./components/PracticeComp2";
 import PracticeComp3 from "./components/PracticeComp3";
+import ComplexJsx from "./components/ComplexJsx";
 
 function App() {
   document.getElementById('root')
@@ -14,6 +15,7 @@ function App() {
       <PracticeComp1></PracticeComp1>
       <PracticeComp2></PracticeComp2>
       <PracticeComp3> </PracticeComp3>
+      <ComplexJsx></ComplexJsx>
     </div>
   );
 }
---
+++ s4/practice-s4/src/components/ComplexJsx.js
@@ -0,0 +1,16 @@
+function ComplexJsx(){
+
+    return (
+        <div> 
+            <h1>Writing More Complex JSX Code </h1>
+            <div>October 18th 2023</div>
+            <div> 
+                <h2>Car Insurance</h2>
+                <div>$294.67</div>
+
+            </div>
+        </div>
+    );
+
+}
+export default ComplexJsx;
