ec0eb652c0d0b096f5b8339e909cb560d5a163a7
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-18 02:15:21 +0600

    #4*44-building a 1st custom component

--- s4/practice-s4/src/App.js
+++ s4/practice-s4/src/App.js
@@ -1,8 +1,12 @@
+
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  document.getElementById('root')
   return (
     <div>
       <h2>Let's get started!</h2>
       <p> This is Sohan & React Js </p>
+      <ExpenseItem> </ExpenseItem>
     </div>
   );
 }
